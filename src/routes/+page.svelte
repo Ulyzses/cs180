0ae5d372052e87ee301e5f7afbd49b0e46f6e9fc
Marius Barcenas <mariusulyzses.barcenas@gmail.com>
2024-05-21 20:51:33 +0800

    feat: remove exposed api call

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,18 @@
-<script>
+<!-- <script>
   export let data;
 
   const emails = data.emails;
+</script> -->
 
-  console.log(emails);
-</script>
+<div class="container">
+  <a href="https://docs.google.com/spreadsheets/d/1uSI-ebIFarr7LTFYXR4zIzkoDYTos__A3sp3jAnkJNQ/edit?usp=sharing">Upload your emails</a>
+</div>
 
-{#each emails as email}
-  <h1>{email.id}</h1>
-  <p>{email.content}</p>
-  <p>{email.email_timestamp}</p>
-{/each}
-
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+</style>
